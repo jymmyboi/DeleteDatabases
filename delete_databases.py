f921b534f9594290620f903eb20d7b571d1d6bb2
James Seton <jamesseton6@gmail.com>
2024-03-23 13:11:33 +1030

    Cleaned up code

--- delete_databases.py
+++ delete_databases.py
@@ -5,17 +5,13 @@ excluded_dbs = []
 db_list = []
 system_dbs = ['master','model','msdb','tempdb']
 
-temp_input = ""
-
 def get_exclusions():
     with open("excluded_dbs.txt", 'r') as file:
         for line in file:
             excluded_dbs.append(line.strip())
 
-
-def get_databases(conn):
+def get_databases(cursor):
     try:
-        cursor = conn.cursor(as_dict=True)
         cursor.execute("SELECT name FROM sys.sysdatabases")
         for row in cursor:
             if row['name'] not in excluded_dbs and row['name'] not in system_dbs:
@@ -24,10 +20,9 @@ def get_databases(conn):
         print("An exception occurred: ", e)
         sys.exit(1)
 
-def delete_databases(conn):
+def delete_databases(cursor):
     try:
         for item in db_list:
-            cursor = conn.cursor()
             cursor.execute("DROP DATABASE " + item + ';')
             print("DROPPED DATABASE " + item)
     except Exception as e:
@@ -40,13 +35,14 @@ def main():
     password = input("Password: ")
     try:
         conn = pymssql.connect(server, user, password, 'master')
+        cursor = conn.cursor(as_dict=True)
     except Exception as e:
         print("An exception occurred: ", e)
         sys.exit(1)
     conn.autocommit(True)
     get_exclusions()
-    get_databases(conn)
-    delete_databases(conn)
+    get_databases(cursor)
+    delete_databases(cursor)
     conn.close()
 
 if __name__ == "__main__":
